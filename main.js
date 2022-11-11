61f27d4aa8d1135d9f73d34d7e5ee94d7ff6ea9c
ecanvasser <eddiecanvassere9@gmail.com>
2022-11-10 16:32:41 -0800

    Add form, buttons, and Add Book button function

---
+++ main.js
@@ -0,0 +1,24 @@
+let myLib = [];
+
+function Book(title, author, pages, read) {
+    this.title = title;
+    this.author = author;
+    this.pages = pages;
+    this.read = read;
+}
+
+Book.prototype.info = function () {
+    return `${this.title} by ${this.author}, ${this.pages}, pages, ${this.read}`;
+}
+
+function addBook(book) {
+    myLib.push(book);
+}
+
+// Add book button function
+document.getElementById('add').onclick = function() {
+    document.getElementById('bookForm').style.display = 'flex';
+    document.querySelector('.formButtons').style.display = 'flex';
+}
+
+// Close Form button function
